b25e3b848956935d663c4b59e92b278fb02f2503
dte-84 <dte.solutions.llc@gmail.com>
2026-09-10 10:02:46 -0500

    ui: shift background image focus to the right

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,8 +1,1 @@
-import { createBrowserClient } from '@supabase/ssr' 
-
-export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
-}
+import { createBrowserClient } from "@supabase/ssr";`n`nconst supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;`nconst supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;`n`nexport const createClient = () =>`n  createBrowserClient(`n    supabaseUrl!,`n    supabaseKey!,`n  );
